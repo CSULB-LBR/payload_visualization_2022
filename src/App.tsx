238b6844d5080a45a6217b8e362cbff549609dda
ashkjones <ashkjones@gmail.com>
2021-10-16 12:04:09 -0700

    Initial React and Babylon App

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PayloadScene from './components/PayloadScene';
+
+function App() {
+  return (
+    <div className="App">
+      <PayloadScene />
+    </div>
+  );
+}
+
+export default App;
